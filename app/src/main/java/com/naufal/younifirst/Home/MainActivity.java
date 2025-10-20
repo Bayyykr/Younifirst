b22787e2eb5e11b5acecd5ee9e939dd5e78b4197
Boii <bayukristanto2005@gmail.com>
2025-10-20 21:05:22 +0700

    Commit revisi struktur tampilan

--- app/src/main/java/com/naufal/younifirst/Home/MainActivity.java
+++ app/src/main/java/com/naufal/younifirst/Home/MainActivity.java
@@ -51,7 +51,7 @@ public class MainActivity extends AppCompatActivity {
     private void replaceFragment(Fragment fragment) {
         FragmentManager fragmentManager = getSupportFragmentManager();
         FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
-        fragmentTransaction.replace(R.id.frame_container,fragment);
+        fragmentTransaction.replace(R.id.fragment_container,fragment);
         fragmentTransaction.commit();
     }
 }
